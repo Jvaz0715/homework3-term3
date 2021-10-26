5f7851ace77d34d7bdd4324b79b153965ea8d786
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-10-26 16:13:07 -0400

    clear all works

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
 import React, { Component } from 'react';
 import Header from './components/Header/Header';
 import Counter from './components/Counter/Counter';
+import Stars from './components/Stars/Stars';
 
 import "./App.css";
 
 export class App extends Component {
   state = {
     counter: 0,
+    starsArray: [],
   }
 
   removeOnClick = () => {
     console.log("remove one star clicked");
     if (this.state.counter > 0) {
+      let lessStarsArray = [...this.state.starsArray];
+      lessStarsArray.pop();
       this.setState({
         counter: this.state.counter - 1,
+        starsArray: lessStarsArray,
       })
     }
   };
 
   addOnClick = () => {
     console.log("add one star clicked");
+    let moreStarsArray = [...this.state.starsArray, "star"];
+  
     this.setState({
       counter: this.state.counter + 1,
+      starsArray: moreStarsArray,
     })
+  }
+
+  clearAllOnClick = () => {
+    this.setState({
+      counter: 0,
+      starsArray: [],
+    });
   }
 
   render() {
@@ -34,6 +49,10 @@ export class App extends Component {
           counter={this.state.counter}
           removeOnClick={this.removeOnClick}
           addOnClick={this.addOnClick}
+        />
+        <Stars 
+          starsArray={this.state.starsArray}
+          clearAllOnClick={this.clearAllOnClick}
         />
       </div>
     )
